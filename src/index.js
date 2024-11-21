d3cdcb9e0a5132f4568da9659f4fcfc35596a800
iam1513 <126111227+iam1513@users.noreply.github.com>
2024-11-22 01:01:22 +0530

    Added logic related to creating a token and verifying then token in service layer.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,26 @@ const app = express()
 const bodyParser = require("body-parser")
 const apiRoutes = require("./routes")
 
+// const UserService = require("./services/user-service")
+
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/api', apiRoutes)
 
-app.listen(ServerConfig.PORT, () => {
-    console.log(`Server started on ${ServerConfig.PORT}`)
-})
+
+function startServer() {
+    app.listen(ServerConfig.PORT, async () => {
+        console.log(`Server started on ${ServerConfig.PORT}`)
+
+        // const userService = new UserService()
+        // const token = await userService.createToken({ email: "om3@gmail.com", id: 6 })
+
+        // console.log("new token : ", token)
+        // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im9tM0BnbWFpbC5jb20iLCJpZCI6NiwiaWF0IjoxNzMyMjE2OTA5LCJleHAiOjE3MzIyMjA1MDl9.GOVLJ9HLwtXpr77Mla1O3Yd-wug_H99PUYIOBP0PzcA"
+        // const res = await userService.verifyToken(token, ServerConfig.JWT_KEY)
+        // console.log(res)
+    })
+}
+
+startServer()
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,5 +1,6 @@
 const UserRepository = require("../repository/user-repository")
-
+const jwt = require("jsonwebtoken")
+const { ServerConfig } = require("../config")
 class UserService {
     constructor() {
         this.userRepository = new UserRepository()
@@ -10,7 +11,27 @@ class UserService {
             const user = await this.userRepository.create(data)
             return user
         } catch (error) {
-            console.log("Something went wrong in Repository.")
+            console.log("Something went wrong in Service.")
+            throw (error)
+        }
+    }
+
+    async createToken(user) {
+        try {
+            const token = jwt.sign(user, ServerConfig.JWT_KEY, { expiresIn: "1h" })
+            return token
+        } catch (error) {
+            console.log("Something went wrong in Token creation.")
+            throw (error)
+        }
+    }
+
+    async verifyToken(token) {
+        try {
+            const response = jwt.verify(token, ServerConfig.JWT_KEY)
+            return response
+        } catch (error) {
+            console.log("Something went wrong in Token validation.")
             throw (error)
         }
     }
